c0bb71594a016be598baa1dcc7c8800a5b707057
Henrique <henrique.falleiros@gmail.com>
2024-11-07 13:44:56 -0300

    alterada a frase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
 
-    return <h1>"Meu coração rebateu, estava dizendo que o velho era sempre novo."(Rosa, João Guimarães. Grande Sertão Veredas.)</h1>
+    return <h1>"Meu coração rebateu, estava dizendo que o velho era sempre novo."(Rosa, João Guimarães. Grande Sertão Veredas.)87)</h1>
 }
 
 export default Home;
