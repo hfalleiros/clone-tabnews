3e45eaac16bbb1512fa4d2c62492630baf18f1ff
Henrique <henrique.falleiros@gmail.com>
2024-11-07 14:34:03 -0300

    inserção de outra citação

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 function Home(){
 
-    return <h1>"Meu coração rebateu, estava dizendo que o velho era sempre novo."(Rosa, João Guimarães. Grande Sertão Veredas.)87)</h1>
+    return <h1>"Meu coração rebateu, estava dizendo que o velho era sempre novo."(Rosa, João Guimarães. Grande Sertão Veredas.)87)<br></br><br></br>"A realidade é dolorosa demais para suportar então a gente distorce ela até que seja suportável."(Munger, Charlie)</h1>
+    
+    
 }
 
 export default Home;
