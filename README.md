1162faf6d271fb0060e0bd34ce7ed940037c8329
Henrique Falleiros <henrique.falleiros@gmail.com>
2024-11-27 23:07:08 -0300

    adiciona scrips  e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório criado para acompanhamento do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-function Home(){
-
-    return <h1>"Meu coração rebateu, estava dizendo que o velho era sempre novo."(Rosa, João Guimarães. Grande Sertão Veredas.)87)<br></br><br></br>"A realidade é dolorosa demais para suportar então a gente distorce ela até que seja suportável."(Munger, Charlie)</h1>
-    
-    
+function Home() {
+  return (
+    <h1>
+      "Meu coração rebateu, estava dizendo que o velho era sempre novo."(Rosa,
+      João Guimarães. Grande Sertão Veredas.)87)<br></br>
+      <br></br>"A realidade é dolorosa demais para suportar então a gente
+      distorce ela até que seja suportável."(Munger, Charlie)
+    </h1>
+  );
 }
 
 export default Home;
