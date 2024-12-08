a4d53fda558116378b6ce635d10b58540a570eb1
Henrique Falleiros <henrique.falleiros@gmail.com>
2024-12-08 14:33:05 -0300

    Adicionada citação do Bob Dylan

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ function Home() {
       "Meu coração rebateu, estava dizendo que o velho era sempre novo."(Rosa,
       João Guimarães. Grande Sertão Veredas.)87)<br></br>
       <br></br>"A realidade é dolorosa demais para suportar então a gente
-      distorce ela até que seja suportável."(Munger, Charlie)
+      distorce ela até que seja suportável."(Munger, Charlie)<br></br>
+      <br></br>"The highest purpose of art is inspire. What else can you do for
+      anyone but inspire then?"(Bob Dylan)
     </h1>
   );
 }
