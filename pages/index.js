34033188a12c90ed2dafa8cfbba105a9cb8556fe
Henrique Falleiros <henrique.falleiros@gmail.com>
2024-12-29 16:54:28 -0300

    Instalado Test Runner e Criado Teste Automatizado.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 function Home() {
   return (
     <h1>
-      "Meu coração rebateu, estava dizendo que o velho era sempre novo."(Rosa,
-      João Guimarães. Grande Sertão Veredas.)87)<br></br>
+      "Meu coração rebateu, estava dizendo que o velho era sempre novo."(João
+      Guimarães Rosa)<br></br>
       <br></br>"A realidade é dolorosa demais para suportar então a gente
-      distorce ela até que seja suportável."(Munger, Charlie)<br></br>
+      distorce ela até que seja suportável."(Charlie Munger)<br></br>
       <br></br>"The highest purpose of art is inspire. What else can you do for
-      anyone but inspire then?"(Bob Dylan)
+      anyone but inspire then?"(Bob Dylan)<br></br>
+      <br></br>
+      "Amar é dar o que você não tem a quem não pediu." (Jacques Lacan)
     </h1>
   );
 }
